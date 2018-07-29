8752f2c040d18b494fa22fdd074ee1ee32312ad8
Dorian Tudorache <dorian.snaz@gmail.com>
2018-07-30 01:28:36 +0300

    ts <3

--- lodger/helpers/forms.ts
+++ lodger/helpers/forms.ts
@@ -1,15 +1,64 @@
-import { RxSchema } from 'rxdb'
-// import { FormData } from '../typings/forms'
+import { RxJsonSchema, RxCollection } from 'rxdb'
+import { FormData } from '../typings/forms'
 
-export class Form {
-  name: string
-  constructor(formName: string) {
-    this.name = formName
-  }
-  toRxSchema(this: Form): RxSchema {
+export class FormError extends Error {
+  constructor(m: string) {
+    super(m)
 
+    // Set the prototype explicitly.
+    Object.setPrototypeOf(this, FormError.prototype)
   }
 }
 
+enum FormErrors {
+  invalidRequested = 'Invalid form requested.',
+  noData = 'Form is missing data'
+}
+type Without<T, K> = {
+  [L in Exclude<keyof T, K>]: T[L]
+}
+type ExcludedOverwrites = 'properties' | 'required' | 'title'
+type AllowedSchemaOverwrites = Without<RxJsonSchema, ExcludedOverwrites>
 
-// const asociatiiSchema = asociatii.toRxSchema()
+export class Form {
+  readonly data?: FormData
+  readonly _schema?: RxJsonSchema
+  readonly _collection?: RxCollection
+
+  constructor(name: string) {
+    try {
+      this.data = Object.assign(require(`../forms/${name}`), { name })
+    } catch (e) {
+      throw new FormError(FormErrors.invalidRequested)
+    }
+
+    this._schema = this.toRxSchema()
+  }
+  private toRxSchema(overwrites?: AllowedSchemaOverwrites): RxJsonSchema {
+    const { data } = this
+    if (!data) {
+      throw new FormError(FormErrors.noData)
+    }
+    const { name } = data
+    const startingSchema: RxJsonSchema = {
+      type: 'object',
+      version: 0,
+      title: name,
+      // name,
+      // autoMigrate: true,
+      properties: {},
+      required: []
+    }
+
+    const schema = startingSchema
+
+    if (overwrites) {
+      Object.assign(schema, overwrites)
+    }
+
+    return schema
+  }
+  toRxCollection(): RxCollection {
+    
+  }
+}

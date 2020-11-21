cc51409964174961d441fce080d138acd26335ff
Dorian Tudorache <dorian.snaz@gmail.com>
2020-11-21 15:34:56 +0200

    teesting new grounds

--- main.js
+++ main.js
@@ -1,27 +1,31 @@
 const { loadNuxt, build } = require('nuxt')
-const server = require('express')()
-const electron = require('electron')
-const path = require('path')
-const url = require('url')
+// const server = require('express')()
+const { app, BrowserWindow } = require('electron')
+const serve = require('electron-serve');
 
-const http = require('http')
+const path = require('path')
+// const url = require('url')
+
+// const http = require('http')
 
 // Import and Set Nuxt.js options
 const config = require('./nuxt.config')
 const isDev = process.env.NODE_ENV !== 'production'
 
-const port = 31337
-let win = null
+const loadURL = serve({ directory: '.' })
 
-const servurl = 'http://localhost:' + port
-const genurl = url.format({
-  pathname: path.join(__dirname, 'dist/index.html'),
-  protocol: 'file:',
-  slashes: true
-})
+// const port = 31337
+let win
 
-const app = electron.app
-const bw = electron.BrowserWindow
+// const servurl = 'http://localhost:' + port
+// const genurl = url.format({
+//   pathname: path.join(__dirname, 'index.html'),
+//   protocol: 'file:',
+//   slashes: true
+// })
+
+// const app = electron.app
+// const bw = electron.BrowserWindow
 
 // Get a ready to use Nuxt instance
 loadNuxt(isDev ? 'dev' : 'start').then(nuxt => {
@@ -29,23 +33,24 @@ loadNuxt(isDev ? 'dev' : 'start').then(nuxt => {
     config.dev = true
     build(nuxt)
   }
-  server.use(nuxt.render)
-  server.listen(port, '127.0.0.1')
-  console.log('Server listening on ' + servurl)
+  // server.use(nuxt.render)
+  // server.listen(port, '127.0.0.1')
+  // console.log('Server listening on ' + servurl)
 
-  const POLL_INTERVAL = 300
-  const pollServer = () => {
-    http.get(servurl, (res) => {
-      const SERVER_DOWN = res.statusCode !== 200
-      SERVER_DOWN ?
-        setTimeout(pollServer, POLL_INTERVAL) :
-        win.loadURL(genurl)
-    })
-      .on('error', pollServer)
-  }
+  // const POLL_INTERVAL = 300
+  // const pollServer = () => {
+  //   console.log('polling')
+  //   http.get(servurl, (res) => {
+  //     const SERVER_DOWN = res.statusCode !== 200
+  //     SERVER_DOWN ?
+  //       setTimeout(pollServer, POLL_INTERVAL) :
+  //       win.loadURL(genurl)
+  //   })
+  //     .on('error', pollServer)
+  // }
 
   const newWin = () => {
-    win = new bw({
+    win = new BrowserWindow({
       frame: false,
       titleBarStyle: 'hiddenInset',
       width: 1024,
@@ -54,11 +59,11 @@ loadNuxt(isDev ? 'dev' : 'start').then(nuxt => {
         webSecurity: false
       }
     })
-    console.log('started', genurl)
+    // console.log('started', genurl)
     // if (isDev) {
     //   return win.loadURL(servurl)
     // }
-    win.loadURL(genurl)
+    loadURL(win)
     // win.on('closed', () => win = null)
     // pollServer()
     // win.loadFile('dist/index.html')
